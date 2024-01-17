783ce3997f832a154711348980feff98dc2f93a4
thanhdatnasdaq <122451214+thanhdatnasdaq@users.noreply.github.com>
2024-01-18 00:55:33 +1100

    Add files via upload

---
+++ controller/EmployeeController.java
@@ -0,0 +1,52 @@
+package net.javaguides.emsbackend.controller;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.emsbackend.dto.EmployeeDto;
+import net.javaguides.emsbackend.entity.Employee;
+import net.javaguides.emsbackend.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+    // Build Add Employee Rest API
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+    // Build Get Employee Rest API
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId){
+        EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
+        return ResponseEntity.ok(employeeDto);
+    }
+
+    // Build Get All Employees Rest API
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees(){
+        List<EmployeeDto> employees = employeeService.getAllEmployees();
+        return  ResponseEntity.ok(employees);
+    }
+
+    // Build Update Employees Rest API
+    @PutMapping("{id}")
+    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("id") Long employeeId, @RequestBody EmployeeDto updatedEmployee){
+        EmployeeDto employeeDto = employeeService.updateEmployee(employeeId, updatedEmployee);
+        return ResponseEntity.ok(employeeDto);
+    }
+
+    // Build Delete Employees Rest API
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId){
+        employeeService.deleteEmployee(employeeId);
+        return ResponseEntity.ok("Employee deleted successfully!");
+    }
+}
---
+++ service/EmployeeService.java
@@ -0,0 +1,17 @@
+package net.javaguides.emsbackend.service;
+
+import net.javaguides.emsbackend.dto.EmployeeDto;
+
+import java.util.List;
+
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long id);
+
+    List<EmployeeDto> getAllEmployees();
+
+    EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee);
+
+    void deleteEmployee(Long employeeId);
+}
